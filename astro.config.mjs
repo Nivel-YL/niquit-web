6f2c303bc15a520d06987b4c8e7bb6e334aa2dfe
Jelissei <jelissei.levin@gmail.com>
2026-07-07 13:08:38 +0300

    feat: set site to niquit.app, add redirects from pages.dev and www

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://niquit.app',
   vite: {
     plugins: [tailwindcss()],
   },
